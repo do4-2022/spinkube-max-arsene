cf79c021a5c44fb66c1362240be02bd8d47c6332
Maxtho <maxime.pizzolitto.82@gmail.com>
2024-04-22 13:27:36 +0200

    feat: install cluster with spinkube

--- terraform/ressources/compute_instances.tf
+++ terraform/ressources/compute_instances.tf
@@ -7,4 +7,60 @@ resource "openstack_compute_instance_v2" "instance" {
     network {
         name = "main"
     }
+    user_data = <<-EOF
+    #!/bin/bash
+    curl -sfL https://get.k3s.io | sh -
+    curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.20.0/bin/linux/amd64/kubectl
+    chmod +x ./kubectl
+    sudo mv ./kubectl /usr/local/bin/kubectl
+
+    sudo su ubuntu
+    cd /home/ubuntu
+    mkdir -p .kube/
+    sudo cp /etc/rancher/k3s/k3s.yaml .kube/config  && sudo chmod +r .kube/config && export KUBECONFIG=.kube/config
+    
+    # Install helm
+    curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
+    chmod 700 get_helm.sh
+    ./get_helm.sh
+    
+    # Install spin-operator CRDs
+
+    kubectl apply -f https://github.com/spinkube/spin-operator/releases/download/v0.1.0/spin-operator.crds.yaml
+    kubectl apply -f https://github.com/spinkube/spin-operator/releases/download/v0.1.0/spin-operator.runtime-class.yaml
+    kubectl apply -f https://github.com/spinkube/spin-operator/releases/download/v0.1.0/spin-operator.shim-executor.yaml
+    
+    # Install cert-manager
+
+    kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.14.3/cert-manager.crds.yaml
+
+    helm repo add jetstack https://charts.jetstack.io
+    helm repo update
+    helm install \
+    cert-manager jetstack/cert-manager \
+    --namespace cert-manager \
+    --create-namespace \
+    --version v1.14.4
+
+    # Install kwasm-operator
+
+    helm repo add kwasm http://kwasm.sh/kwasm-operator/
+    helm install \
+    kwasm-operator kwasm/kwasm-operator \
+    --namespace kwasm \
+    --create-namespace \
+    --set kwasmOperator.installerImage=ghcr.io/spinkube/containerd-shim-spin/node-installer:v0.13.1
+    kubectl annotate node --all kwasm.sh/kwasm-node=true
+    
+    # Install spin-operator
+
+    helm install spin-operator \
+    --namespace spin-operator \
+    --create-namespace \
+    --version 0.1.0 \
+    --wait \
+    oci://ghcr.io/spinkube/charts/spin-operator
+
+    kubectl apply -f https://raw.githubusercontent.com/spinkube/spin-operator/main/config/samples/simple.yaml
+    EOF
 }
